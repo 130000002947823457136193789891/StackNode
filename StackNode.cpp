86e7a2fa5c37560b7cbc535d3631f91e826fec6c
胡雅源 <1677080339@qq.com>
2018-09-29 19:34:25 +0800

    Initial commit

---
+++ StackNode.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+typedef int DataType;
+class StackNode
+{
+public:
+	DataType data;
+	StackNode *next;
+	StackNode()
+	{next=NULL;
+	};
+};
+
+int main()
+{
+	return 0;
+}
